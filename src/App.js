c68eb084c17e12cbf90538a3a61b2619d0de3bca
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2020-11-01 11:57:30 +0700

    create component structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import Playground from './pages/Playground';
 
 function App() {
   return (
     <div className="App">
-
+      <Switch>
+        <Route exact path="/home" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/playground" component={Playground} />
+        <Route />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/Playground.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Playground() {
+    return (
+        <div>
+            Playground
+        </div>
+    )
+}
+
+export default Playground
