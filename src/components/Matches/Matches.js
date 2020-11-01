a4630e2fe347328165f6a1a2bfb49cec8debbedb
jay <chairit@parbpim.com>
2020-11-01 14:19:37 +0700

    start doing matches component

--- src/components/Matches/Matches.js
+++ src/components/Matches/Matches.js
@@ -5,7 +5,13 @@ export default function Matches() {
   return (
     <div className="matches">
       <div className="matches-header">
-        <a href="#">My Profile</a>
+        <a className="matches-header__profile" href="#">
+          <img src="./images/profile.jpg" alt="" />
+          <span>My Profile</span>
+        </a>
+        <button>
+          <i class="fas fa-shopping-bag"></i>
+        </button>
       </div>
     </div>
   );
