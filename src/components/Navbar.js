89eb6d61b33b89f5a5f0d81e3127039f84174e4e
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2020-11-01 13:29:37 +0700

    create homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,30 @@
-import React from 'react'
 
-function Navbar() {
+import React, { useState } from 'react';
+import { Affix, Button, Col, Row } from 'antd';
+import { withRouter } from 'react-router-dom';
+
+function Navbar(props) {
+    const [top, setTop] = useState(10);
     return (
-        <div>
-            Navbar
-        </div>
+        <>
+            <Affix offsetTop={top} >
+                <Row justify="space-around" align="middle" style={{ border: "1px solid" }} >
+                    <Col span={4} style={{ border: "1px solid" }} >
+                        <div style={{ fontSize: "2.5rem" }} >
+                            <i class="fas fa-heartbeat"></i>
+                            <span>Dating</span>
+                        </div>
+                    </Col>
+                    <Col span={20} style={{ display: "flex", justifyContent: "flex-end" }} >
+                        <Button type="primary" onClick={() => props.history.push('/login')}>
+                            Login
+                    </Button>
+                    </Col>
+                </Row>
+
+            </Affix>
+        </>
     )
 }
 
-export default Navbar
+export default withRouter(Navbar);
